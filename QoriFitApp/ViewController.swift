d51990533144aa9d104fe4a8387ae3e5efa28903
LifoX404 <ccorrales.dev@outlook.com>
2026-04-09 21:12:18 -0500

    Initial Commit

---
+++ QoriFitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QoriFitApp
+//
+//  Created by Apple on 9/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
